207cae3b011d43c90a6e5e296ecca1c95010e097
sonamsherpatech <sonam.sherpa.tech@gmail.com>
2025-07-23 16:13:36 +0545

    add basic ts concept like array, annotation, inference, and so on

---
+++ src/challenge.ts
@@ -0,0 +1,49 @@
+/*
+///////////////////////////////
+//////   Challenge - 1 ////////
+//////////////////////////////
+let fullName = "Sonam Sherpa";
+console.log(fullName.toLowerCase());
+// fullName = 23
+// fullName = false
+
+let value = 35;
+value = 34 + 21;
+console.log(value);
+// value = "Help"
+// value = true
+
+let isValue = false;
+if(isValue) isValue= false
+else isValue = true
+console.log(isValue)
+// isValue = "true"
+// isValue = 1
+
+///////////////////////////////
+/////// Challenge - 2 ////////
+//////////////////////////////
+
+let orderStatus:'processing' | 'shipped' | 'delivered' = 'processing';
+
+orderStatus = "shipped"
+orderStatus = "delivered"
+// orderStatus = "cancelled"
+
+let discount : number | string = 20
+discount = '20%'
+// discount = true
+*/
+
+///////////////////////////////
+/////// Challenge - 3 ////////
+//////////////////////////////
+
+let temperature: number[] = [32, 31, 34];
+// temperature.push('thirty-six')
+
+let colors: string[] = ["Red", "White", "Blue"];
+// colors.push(00000)
+
+let mixedArray: (number | string)[] = [32, "Red", 33, "Blue"];
+// mixedArray.push(true)
---
+++ src/tutorial.ts
@@ -0,0 +1,68 @@
+//arrays in ts
+let prices: number[] = [100, 75, 42];
+// prices.push('df')
+
+let fruit: string[] = ["apple", "orrange"];
+// fruit.push(23)
+
+// let randomValues:[] = ['hello']
+let emptyValues: number[] = [];
+
+let names = ['Rohan','milan',1,true]
+
+let array : (string | boolean)[] = ['apple',true,'orange',false]
+
+/* 
+console.log("type script tutorial");
+
+let awesomeName = "Sonam Sherpa";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+// awesomeName = 20
+
+let amount  = 20
+amount = 12 - 1
+// amount = "Pants"
+
+let isAwesome = true
+isAwesome = false
+// isAwesome = 'Sonam Sherpa'
+console.log(amount, isAwesome)
+
+//Union types
+let tax:number | string  = 10
+tax = 100
+tax = "$10"
+console.log(tax)
+// tax = false
+
+let requestStatus: 'pending' | 'success' | 'error' = 'pending'
+requestStatus = 'success'
+requestStatus = 'error'
+console.log(requestStatus)
+// requestStatus = 'random'
+
+//any type
+let notSure:any = 4
+notSure = "Sonam"
+notSure = true
+
+//Practical application of type annotation
+
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === "1983") {
+    foundBook = book;
+    foundBook = foundBook.toUpperCase();
+    break;
+  }
+}
+// foundBook= 10
+// console.log(foundBook);
+
+console.log(foundBook?.length);
+*/
